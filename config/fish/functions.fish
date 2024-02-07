72d7112b7fc50688c89d16d43dc57eef60d79123
_ <no@email>
2024-02-07 13:17:05 +0100

    added a lot of abbreviations and functions in fish, and also some micro plugins (all of them, actually)

--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -1,3 +1,25 @@
+function junk
+    bat -pp /usr/share/dict/spanish | shuf | tail -n $argv[1] | tr \n " "
+end
+
+function l
+    lsblk && \
+    echo && \
+    df -h -x tmpfs -x efivarfs | sed '2d'
+end
+
+function quote_all
+    grep " No\.[0-9][0-9][0-9][0-9]" $argv[1] | cut -c 35-42 | sed 's/\./>>/g' | tr \n " "
+end
+
+function remove_comments
+    sed -i 's#//.*$##g' $argv[1]
+end
+
+function bs 
+    echo \\ Ctrl+U 005C; echo -n "\\" | xclip -selection clipboard && xmodmap -e "clear Lock" -e "keycode 66 = backslash"
+end
+
 function lst
     find . -maxdepth 1 -type f -exec sh -c 'echo -e "$(basename "{}")  $(file --mime-type -b "{}")"' \;
 end
@@ -47,8 +69,8 @@ function t
         rm -rf /tmp/t/*
         echo "deleted all files inside /tmp/t"
     else
-        set random_chars (tr -dc "a-z" </dev/urandom | head -c 10)
-        set temp_dir "/tmp/t/$argv[1]_$random_chars"
+        set random_chars (date +"%s%N")
+        set temp_dir "/tmp/t/$argv[1]__$random_chars"
         mkdir -p "$temp_dir"; and cd "$temp_dir"
     end
 end
@@ -69,28 +91,36 @@ function zst
     else
         set -x ZSTD_CLEVEL 19
     end
-    tar --zstd -cvf  "$argv[1].tar.zst" $argv[1]
+
+    if string match -q "*/" "$argv[1]"
+        set -x argv[1] (string split "" $argv[1] | head -n -1 | string join "")
+    end
+
+    tar --zstd -cvf "$argv[1].tar.zst" "$argv[1]"
 end
+
 
 function ex --description "Expand or extract bundled & compressed files"
   set --local ext (echo $argv[1] | awk -F. '{print $NF}')
   switch $ext
-    case tar  # non-compressed, just bundled
+    case tar  
       tar -xvf $argv[1]
     case gz
-      if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar  # tar bundle compressed with gzip
+      if test (echo $argv[1] | awk -F. '{print $(NF-1)}') = tar 
         tar -zxvf $argv[1]
-      else  # single gzip
+      else  
         gunzip $argv[1]
       end
-    case tgz  # same as tar.gz
+    case tgz
       tar -zxvf $argv[1]
-    case bz2  # tar compressed with bzip2
+    case bz2
       tar -jxvf $argv[1]
     case rar
       unrar x $argv[1]
     case zip
       unzip $argv[1]
+    case zst
+      zstd -d $argv[1]
     case '*'
       echo "unknown extension"
   end
