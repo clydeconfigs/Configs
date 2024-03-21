95b33b54da89c6cd2480fcf1ef2316b4dc495cb8
clydeconfigs <memeit2024@gmail.com>
2024-03-21 16:34:36 +0100

    epic changes

--- config/fish/abbreviations.fish
+++ config/fish/abbreviations.fish
@@ -1,10 +1,7 @@
 # abbreviations
-abbr -ag top 'bpytop'
-abbr -ag lb 'lsblk'
-abbr -ag gu 'gitui'
+abbr -ag feh 'feh -d  --scale-down'
 abbr -ag gpg 'gpg --pinentry-mode loopback'
 abbr -ag copy 'xclip -sel c'
-abbr -ag vi 'nvim'
 abbr -ag upper 'tr [:lower:] [:upper:]'
 abbr -ag lower 'tr [:upper:] [:lower:]'
 abbr -ag tb 'nc termbin.com 9999'
@@ -17,12 +14,16 @@ abbr -ag ... 'cd .. && cd ..'
 abbr -ag .... 'cd .. && cd .. && cd ..'
 abbr -ag ..... 'cd .. && cd .. && cd .. && cd ..'
 
+# short
+abbr -ag lb 'lsblk'
+abbr -ag gu 'gitui'
 abbr -ag m 'micro'
 abbr -ag em 'emacs -nw'
 abbr -ag ra 'ranger'
 abbr -ag p 'python3'
 abbr -ag ju 'julia'
 
+# coreutils options
 abbr -ag base64 'base64 -w0' 
 abbr -ag du 'du -h'
 abbr -ag ls 'ls --color=auto'
@@ -37,6 +38,8 @@ abbr -ag u 'yay -Syu'
 abbr -ag s 'yay'
 
 # i3 
+abbr -ag abbreviations.fish 'micro ~/.config/fish/abbreviations.fish'
+abbr -ag functions.fish 'micro ~/.config/fish/functions.fish'
 abbr -ag config 'micro ~/.config/i3/config'
 abbr -ag config2 'micro ~/.config/i3status/config'
 
@@ -44,3 +47,10 @@ abbr -ag config2 'micro ~/.config/i3status/config'
 abbr -ag ai 'ollama run llama2:13b --nowordwrap'
 abbr -ag ai2 'ollama run tinyllama:1.1b --nowordwrap'
 abbr -ag aimini 'ollama run qwen:0.5b --nowordwrap'
+
+# navigation
+abbr -ag media 'cd ~/Media'
+abbr -ag shell 'cd ~/Media/Code/shell'
+abbr -ag rofi 'cd ~/Media/Code/rofi'
+abbr -ag pics 'cd ~/Media/Random/Pics'
+abbr -ag rando 'cd ~/Media/Random'
