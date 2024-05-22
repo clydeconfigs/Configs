e2ef2f3601a5105ad3ad7b838d4e4c0710c30e0e
clydeconfigs <memeit2024@gmail.com>
2024-05-22 13:17:22 +0200

    .

--- builds/st/config.h
+++ builds/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=19:antialias=true:autohint=true";static int borderpx = 2;
+static char *font = "Terminus:pixelsize=22:antialias=true:autohint=true";static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
